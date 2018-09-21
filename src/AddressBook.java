6c1d11ac62272afcb3595c8d6934693d11d5c121
Ahmed <ahmedromih@cmail.carleton.ca>
2018-09-21 09:25:15 -0400

    Added files

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
 
 public class AddressBook {
+	
+	private ArrayList<BuddyInfo> buddies;
+	
+	
+	public void addBuddy(BuddyInfo buddy) {
+		
+		if(buddy != null) {
+			buddies.add(buddy);
+		}
+		
+	}
+	
+	public void removeBuddy(int index) {
+		if(index >= 0 && index < buddies.size()) {
+			buddies.remove(index);
+		}
+	}
+	
+	public static void main(String[] args) {
+        System.out.println("Address Book");
 
+        BuddyInfo bud = new BuddyInfo("Tom", "111", "Carleton");
+
+        AddressBook book = new AddressBook();
+        book.addBuddy(bud);
+        book.removeBuddy(0);
+	}
 }
