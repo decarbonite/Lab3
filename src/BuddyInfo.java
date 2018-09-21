b1acf5b92cd66edfd4cb5255c33edb9753f5c953
Ahmed <ahmedromih@cmail.carleton.ca>
2018-09-21 09:35:09 -0400

    Added buddyinfo

---
+++ src/BuddyInfo.java
@@ -0,0 +1,50 @@
+public class BuddyInfo {
+	
+	private String name;
+	
+	public BuddyInfo(String name, String phone, String address) {
+		
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+	}
+
+
+
+	private String phone;
+	private String address;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
